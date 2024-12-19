fce98969d7e77d2fdf7c6df445c4ce8385ae4616
Ali Cetin <105593941+420ali@users.noreply.github.com>
2024-12-19 23:09:52 +0300

    Update settings.py

--- alysoftware/settings.py
+++ alysoftware/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "alysoftware", "staticfiles")
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
