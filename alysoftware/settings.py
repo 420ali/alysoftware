ff08bf7e575816f44ecbcc68feb4561f5d09863c
Ali Cetin <105593941+420ali@users.noreply.github.com>
2024-12-19 17:35:12 +0300

    Update settings.py

--- alysoftware/settings.py
+++ alysoftware/settings.py
@@ -114,6 +114,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
